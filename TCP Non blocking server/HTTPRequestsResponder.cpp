817832edd47731d40c08aa9f770d82fba3eb24d0
Omer Cohen Shor <omercs8@gmail.com>
2023-01-04 15:45:23 +0200

    head

--- TCP Non blocking server/HTTPRequestsResponder.cpp
+++ TCP Non blocking server/HTTPRequestsResponder.cpp
@@ -12,7 +12,7 @@ HTTPRequestsResponder::eLanguage HTTPRequestsResponder::get_lang_query_param(str
 
 string HTTPRequestsResponder::create_path_to_file(string endpoint, eLanguage lang)
 {
-	string path_to_relative_root = string(__FILE__).substr(0, string(__FILE__).find_last_of('\\'));
+	string path_to_file;
 	string full_file_name = endpoint.substr(endpoint.find_last_of('/'));
 	string file_name = full_file_name.find('.') != string::npos? full_file_name.substr(0, full_file_name.find('.')) : full_file_name;
 	string file_extension = full_file_name.find('.') != string::npos ? full_file_name.substr(full_file_name.find('.')) : "";
@@ -22,47 +22,88 @@ string HTTPRequestsResponder::create_path_to_file(string endpoint, eLanguage lan
 	switch (lang)
 	{
 	case HTTPRequestsResponder::eLanguage::EN:
-		path_to_relative_root += relativ_file_position + file_name +"_EN" + file_extension;
+		path_to_file = ROOT_FOLDER + relativ_file_position + file_name +"_EN" + file_extension;
 		break;
 	case HTTPRequestsResponder::eLanguage::HE:
-		path_to_relative_root += relativ_file_position + file_name +"_HE" + file_extension;
+		path_to_file = ROOT_FOLDER + relativ_file_position + file_name +"_HE" + file_extension;
 		break;
 	case HTTPRequestsResponder::eLanguage::FR:
-		path_to_relative_root += relativ_file_position + file_name +"_FR" + file_extension;
+		path_to_file = ROOT_FOLDER + relativ_file_position + file_name +"_FR" + file_extension;
 		break;
 	}
 
-	return path_to_relative_root;
+	return path_to_file;
 }
 
-string HTTPRequestsResponder::get_body_from_file_and_return_status(string path_to_file, string& response_status)
+string HTTPRequestsResponder::get_body_from_file_and_update_status_and_type(string path_to_file, string& response_status, string& content_type)
 {
-	string path_to_not_found_file = string(__FILE__).substr(0, string(__FILE__).find_last_of('\\')) + "\\pages\\endpoint_not_exist.html";
 	string file_content;
 	ifstream file(path_to_file);
 
-	if (file.is_open()) {
+	if (file.is_open() && file.good()) {
 		response_status = OK;
 		string content((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
 		file_content = content;
+		content_type += get_extention_to_content_type(path_to_file);
 		file.close();
 	}
 	else
 	{
-		file.open(path_to_not_found_file);
-		if (file.is_open()) {
-			response_status = NOT_FOUND;
-			string content((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
-			file_content = content;
-			file.close();
-		}
-		else
-		{
-			response_status = SERVER_ERROR;
-		}
+		file_content = set_status_404_and_get_404_content(response_status, content_type);
 	}
 
 	return file_content;
+}
+
+string HTTPRequestsResponder::get_extention_to_content_type(string file_path)
+{
+	string file_extention = file_path.find('.') != string::npos ? file_path.substr(file_path.find('.')) : "";
+	string content_type;
+
+	if (file_extention == ".html")
+		content_type = "text/html";
+	else if (file_extention == ".json")
+		content_type = "text/json";
+	else if (file_extention == ".xml")
+		content_type = "application/xml";
+	else if (file_extention == ".js")
+		content_type = "application/javascript";
+	else
+		content_type = "text/plain";
+
+	return content_type;
+}
+
+string HTTPRequestsResponder::set_status_404_and_get_404_content(string& response_status, string& content_type)
+{
+	string file_content;
+	string path_to_not_found_file = ROOT_FOLDER;
+	path_to_not_found_file += PAGE404;
+
+	ifstream file(path_to_not_found_file);
+
+	if (file.is_open() && file.good()) {
+		response_status = NOT_FOUND;
+		string content((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
+		file_content = content;
+		content_type += "text/html";
+		file.close();
+	}
+	else
+	{
+		response_status = SERVER_ERROR;
+		content_type += "text/plain";
+	}
+
+	return file_content;
+}
+
+bool HTTPRequestsResponder::check_if_file_exist_and_not_dir(string file_path)
+{
+	ifstream file(file_path);
+	bool is_file_exist = file.is_open() && file.good();
+	file.close();
+	return is_file_exist;
 }
 
 string HTTPRequestsResponder::print_body_content_and_generate_response_body(string body_content)
@@ -102,8 +143,7 @@ string HTTPRequestsResponder::do_request_and_generate_http_response(HTTPRequestI
 	case eRequestType::GET:
 		lang = get_lang_query_param(httpRequest.query_params);
 		path_to_file = create_path_to_file(httpRequest.endpoint, lang);
-		http_body = get_body_from_file_and_return_status(path_to_file, http_response_status);
-		http_content_Type += "text/html";
+		http_body = get_body_from_file_and_update_status_and_type(path_to_file, http_response_status, http_content_Type);
 		http_content_length += http_body.empty() ? "0" : to_string(http_body.size());
 		generated_response = http_version + " " + http_response_status + "\n" +
 			http_content_Type + "\n" + http_content_length + "\n\n" + http_body;
@@ -117,7 +157,12 @@ string HTTPRequestsResponder::do_request_and_generate_http_response(HTTPRequestI
 			http_content_Type + "\n" + http_content_length + "\n\n" + http_body;
 		break;
 	case eRequestType::HEAD:
-
+		lang = get_lang_query_param(httpRequest.query_params);
+		path_to_file = create_path_to_file(httpRequest.endpoint, lang);
+		http_body = get_body_from_file_and_update_status_and_type(path_to_file, http_response_status, http_content_Type);
+		http_content_length += http_body.empty() ? "0" : to_string(http_body.size());
+		generated_response = http_version + " " + http_response_status + "\n" +
+			http_content_Type + "\n" + http_content_length + "\n\n";
 		break;
 	case eRequestType::PUT:
 
--- TCP Non blocking server/HTTPRequestsResponder.h
+++ TCP Non blocking server/HTTPRequestsResponder.h
@@ -9,6 +9,8 @@ using namespace std;
 #define SERVER_ERROR "500 Internal Server Error";
 #define OK "200 OK";
 #define NOT_IMPLEMENTED "501 Not Implemented";
+#define ROOT_FOLDER "C:\\temp"
+#define PAGE404 "\\pages\\page404.html";
 
 class HTTPRequestsResponder
 {
@@ -21,7 +23,10 @@ private:
 
 	static eLanguage get_lang_query_param(string query_params);
 	static string create_path_to_file(string endpoint, eLanguage lang);
-	static string get_body_from_file_and_return_status(string path_to_file, string& response_status);
+	static string get_body_from_file_and_update_status_and_type(string path_to_file, string& response_status, string& content_type);
+	static string get_extention_to_content_type(string file_path);
+	static string set_status_404_and_get_404_content(string& response_status, string& content_type);
+	static bool check_if_file_exist_and_not_dir(string file_path);
 	static string print_body_content_and_generate_response_body(string body_content);
 
 public:
